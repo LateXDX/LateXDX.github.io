7f50d6f5a1e17518e498a679b7e8a6f50d67940a
Lateeee <55127989+Lateeee@users.noreply.github.com>
2024-11-12 13:59:00 +0200

    emailings

--- common.js
+++ common.js
@@ -18,4 +18,43 @@ function displayClickCounts() {
     document.getElementById('link3-count').innerText = getClickCount('Suru.html');
 }
 
+function checkAndSendEmail() {
+    const now = new Date();
+    const targetHour = 14; // Aika, jolloin sähköposti lähetetään (12:00)
 
+    // Tarkista, onko nykyinen aika klo 12:00
+    if (now.getHours() === targetHour && now.getMinutes() === 0) {
+        sendEmail();
+    }
+}
+
+function sendEmail() {
+
+
+    const link1Count = getClickCount('Ilo.html');
+    const link2Count = getClickCount('JuuJaa.html');
+    const link3Count = getClickCount('Suru.html');
+
+    const emailData = {
+        link1_count: link1Count,
+        link2_count: link2Count,
+        link3_count: link3Count,
+    }
+
+    const serviceID = 'default_service';
+    const templateID = 'template_r263zqt';
+
+    emailjs.send(serviceID,templateID, emailData);
+}
+
+function startEmailScheduler() {
+    // Tarkista heti, että jos sivu ladataan juuri oikeaan aikaan
+    checkAndSendEmail();
+
+    // Aseta ajastin tarkistamaan joka minuutti (60000ms = 1 minuutti)
+    setInterval(checkAndSendEmail, 60000); // Tarkistaa joka minuutti
+}
+
+window.onload = function() {
+    scheduleDailyEmail();
+};
